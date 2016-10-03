de581ca8fa6d2edbf80efaea56258cc8d8a986a8
smeckley <smeckley@cirrus.com>
2016-10-03 18:52:47 -0500

    Updated with small iterattion example

--- first_look_at_the_data_structure.py
+++ first_look_at_the_data_structure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/tools/common/linux/python/3.5.0/bin/python3
 # originially run with python 2.6.6
 # Using the API from https://developers.wargaming.net/reference/
 # Acquired the original data set like this:
@@ -10,7 +10,8 @@
 #     Where the module type can be:
 module_types = ['vehicleRadio', 'vehicleEngine', 'vehicleGun', 'vehicleChassis', 'vehicleTurret']
 #     And the nations can be:
-nations = ['france', 'germany', 'uk', 'usa', 'ussr', 'china', 'japan', 'czech']
+nation_types = ['france', 'germany', 'uk', 'usa', 'ussr', 'china', 'japan', 'czech']
+tank_types = ['heavyTank', 'mediumTank', 'lightTank']
 
 import os, sys, shlex, json, re
 from pprint import pprint
@@ -22,11 +23,15 @@ print( type(json) )
 print( json.keys() )
 wot_veh_dat = json.get('data')
 print( wot_veh_dat.keys() )
+for veh_id, veh_dat in wot_veh_dat.items():
+    if veh_dat['type'] not in tank_types:
+        tank_types.append( veh_dat['type'] )
 wot_veh_dat_6721 = wot_veh_dat.get('6721')
 print( wot_veh_dat_6721.keys() )
 print( wot_veh_dat_6721['guns'] )
 pprint( wot_veh_dat_6721 )
 
 
+print( tank_types )
 
 
